e8793115c000ff3c9b2126beb38f70b64d8a390f
leodario <leodario3@hotmail.com>
2019-12-23 18:06:26 -0300

    atualiza

--- aula070_funcoes_construtoras/index.js
+++ aula070_funcoes_construtoras/index.js
@@ -1,2 +1,12 @@
-// função construtora -> objetos
-// função fábrica -> objetos
+// função construtora -> objetos - faz muita coisa automaticamente, já cria e retorna objeto 
+// função fábrica -> objetos - é uma função normal, faz retornor um objeto voluntariamente
+// Construtora -> temos sempre que iniciar seu nome com letra maiúscula exemplo Pessoa - acessando com o new
+// funções construtoras e fábricas são como moldes
+
+// criando função construtora
+function Pessoa(nome, sobrenome){
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+}
+
+const p1
